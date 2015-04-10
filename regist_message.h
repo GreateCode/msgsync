8c68bff7f67d7612a465f65d111bc8bf02741686
jimingmin <jimingmin@gmail.com>
2015-04-11 00:32:04 +0800

    edit

--- regist_message.h
+++ regist_message.h
@@ -13,11 +13,13 @@
 #include "../include/control_head.h"
 #include "../include/sync_msg.h"
 #include "logic/msgsync_handler.h"
+#include "logic/msgfinack_handler.h"
 
 using namespace FRAME;
 
 MSGMAP_BEGIN(msgmap)
 ON_PROC_PCH_PMH_PMB_PU8_I32(MSGID_MSGSYNC_REQ, ControlHead, MsgHeadCS, CMsgSyncReq, CMsgSyncHandler, CMsgSyncHandler::MsgSync);
+ON_PROC_PCH_PMH_PMB_PU8_I32(MSGID_MSGFINACK_REQ, ControlHead, MsgHeadCS, CMsgFinAckReq, CMsgFinAckHandler, CMsgFinAckHandler::MsgFinAck);
 MSGMAP_END(msgmap)
 
 #endif /* REGIST_MESSAGE_H_ */
